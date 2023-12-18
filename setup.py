f158d9ec2964f62e407a32691bc0d7a3a28f8e27
Aditya Jagadale <aditya.jagdale0110@gmail.com>
2023-12-19 01:51:45 +0530

    requirements and installation done

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarization-End-To-End-With-Deployment"
+AUTHOR_USER_NAME = "AdityaJ9082"
+SRC_REPO = "textsummarizer"
+AUTHOR_EMAIL = "aditya.jagdale0110@gmail.com"
+
+
+#look for constructor file for every folder and it will install it as local package
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
